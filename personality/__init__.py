100fe6df127ed6c6eed493bb8e153d5d72635708
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-26 22:44:48 +0800

    Export personality context helpers

--- personality/__init__.py
+++ personality/__init__.py
@@ -1,3 +1,9 @@
+from .context import HIKARI_PERSONALITY_KEY, personality_as_context
 from .profile import PersonalityProfile, load_personality
 
-__all__ = ["PersonalityProfile", "load_personality"]
+__all__ = [
+    "HIKARI_PERSONALITY_KEY",
+    "PersonalityProfile",
+    "load_personality",
+    "personality_as_context",
+]
