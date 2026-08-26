f1aadf6aa4675f71c927ae7a6d17215ba124ceeb
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-26 23:57:18 +0800

    Export reflection session API

--- learning/__init__.py
+++ learning/__init__.py
@@ -4,10 +4,22 @@ from .reflection import (
     LearningReflectionError,
     LearningReflector,
 )
+from .session import (
+    DEFAULT_REFLECTION_KINDS,
+    LearningSession,
+    LearningSessionResult,
+    LearningSessionState,
+    ReflectionTriggerPolicy,
+)
 
 __all__ = [
     "ALLOWED_LEARNING_KINDS",
+    "DEFAULT_REFLECTION_KINDS",
     "LEARNING_CONTEXT_KEY",
     "LearningReflectionError",
     "LearningReflector",
+    "LearningSession",
+    "LearningSessionResult",
+    "LearningSessionState",
+    "ReflectionTriggerPolicy",
 ]
