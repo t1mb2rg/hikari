9eeb3e3ec0dccf0ae8320df730554d923f462c25
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-26 23:48:02 +0800

    Export reflective learning

---
+++ learning/__init__.py
@@ -0,0 +1,13 @@
+from .reflection import (
+    ALLOWED_LEARNING_KINDS,
+    LEARNING_CONTEXT_KEY,
+    LearningReflectionError,
+    LearningReflector,
+)
+
+__all__ = [
+    "ALLOWED_LEARNING_KINDS",
+    "LEARNING_CONTEXT_KEY",
+    "LearningReflectionError",
+    "LearningReflector",
+]
