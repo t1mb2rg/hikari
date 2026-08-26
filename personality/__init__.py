a445b79c93ee4f24ed0f77e49d764f2c7b65fb5f
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-26 23:39:44 +0800

    Export emotion state interfaces

--- personality/__init__.py
+++ personality/__init__.py
@@ -1,9 +1,23 @@
 from .context import HIKARI_PERSONALITY_KEY, personality_as_context
+from .emotion import (
+    DEFAULT_EMOTION_STATE,
+    EMOTION_DIMENSIONS,
+    HIKARI_EMOTION_KEY,
+    EmotionPolicy,
+    EmotionState,
+    emotion_as_context,
+)
 from .profile import PersonalityProfile, load_personality
 
 __all__ = [
+    "DEFAULT_EMOTION_STATE",
+    "EMOTION_DIMENSIONS",
+    "HIKARI_EMOTION_KEY",
     "HIKARI_PERSONALITY_KEY",
+    "EmotionPolicy",
+    "EmotionState",
     "PersonalityProfile",
+    "emotion_as_context",
     "load_personality",
     "personality_as_context",
 ]
