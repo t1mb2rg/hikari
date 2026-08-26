332f69f1d221c8c1fc67d01a72fc725f0032c24d
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-26 20:13:20 +0800

    Add attention package

---
+++ attention/__init__.py
@@ -0,0 +1,5 @@
+"""Attention primitives for deciding what deserves Hikari's cognition."""
+
+from .policy import AttentionDecision, AttentionPolicy
+
+__all__ = ["AttentionDecision", "AttentionPolicy"]
