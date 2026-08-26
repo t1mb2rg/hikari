bb0e4bfc694579572c7dcc3f1d5f954d730ada47
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-27 00:01:55 +0800

    Export learning assimilation API

--- learning/__init__.py
+++ learning/__init__.py
@@ -1,3 +1,9 @@
+from .assimilation import (
+    DEFAULT_ASSIMILATION_KINDS,
+    LEARNED_CONTEXT_KEY,
+    LearningAssimilationPolicy,
+    learned_memories_as_context,
+)
 from .reflection import (
     ALLOWED_LEARNING_KINDS,
     LEARNING_CONTEXT_KEY,
@@ -14,12 +20,16 @@ from .session import (
 
 __all__ = [
     "ALLOWED_LEARNING_KINDS",
+    "DEFAULT_ASSIMILATION_KINDS",
     "DEFAULT_REFLECTION_KINDS",
+    "LEARNED_CONTEXT_KEY",
     "LEARNING_CONTEXT_KEY",
+    "LearningAssimilationPolicy",
     "LearningReflectionError",
     "LearningReflector",
     "LearningSession",
     "LearningSessionResult",
     "LearningSessionState",
     "ReflectionTriggerPolicy",
+    "learned_memories_as_context",
 ]
