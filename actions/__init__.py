78ce399d4be0053ed054a0d8f55b6b4ea9b12576
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-27 11:29:30 +0800

    Export action authorization API

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,11 +1,21 @@
+from .authorization import (
+    ActionAuthorizationPolicy,
+    AuthorizationDecision,
+    AuthorizationResult,
+    AuthorizedAction,
+)
 from .contract import ActionCatalog, ActionProposal, ActionRisk, ActionSpec
 from .planner import ActionPlanningError, ModelActionPlanner
 
 __all__ = [
+    "ActionAuthorizationPolicy",
     "ActionCatalog",
     "ActionPlanningError",
     "ActionProposal",
     "ActionRisk",
     "ActionSpec",
+    "AuthorizationDecision",
+    "AuthorizationResult",
+    "AuthorizedAction",
     "ModelActionPlanner",
 ]
