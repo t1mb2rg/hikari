51b3e5153d91cbfb2fc315c17861791a61df56c2
t1mb2rg <174198067+t1mb2rg@users.noreply.github.com>
2026-08-28 21:05:31 +0800

    Expose Hikari voice profile

--- personality/__init__.py
+++ personality/__init__.py
@@ -8,6 +8,7 @@ from .emotion import (
     emotion_as_context,
 )
 from .profile import PersonalityProfile, load_personality
+from .voice import VoiceProfile, load_voice
 
 __all__ = [
     "DEFAULT_EMOTION_STATE",
@@ -17,7 +18,9 @@ __all__ = [
     "EmotionPolicy",
     "EmotionState",
     "PersonalityProfile",
+    "VoiceProfile",
     "emotion_as_context",
     "load_personality",
+    "load_voice",
     "personality_as_context",
 ]
